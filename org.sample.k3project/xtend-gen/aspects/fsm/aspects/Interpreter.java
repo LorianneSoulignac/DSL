42ca4f253f4bdb0c0e0d4f49fa1220a6b00ccb42
LorianneSoulignac <lorianne.soulignac@gmail.com>
2020-02-09 18:41:14 +0100

    Add compilator

---
+++ org.sample.k3project/xtend-gen/aspects/fsm/aspects/Interpreter.java
@@ -0,0 +1,15 @@
+package aspects.fsm.aspects;
+
+@SuppressWarnings("all")
+public class Interpreter {
+  public static void main(final String[] args) {
+    throw new Error("Unresolved compilation problems:"
+      + "\nThe method or field fr is undefined"
+      + "\nThe method or field execute is undefined for the type FSM"
+      + "\nut2j cannot be resolved"
+      + "\nm1ice cannot be resolved"
+      + "\nfsm cannot be resolved"
+      + "\neINSTANCE cannot be resolved"
+      + "\nclass cannot be resolved");
+  }
+}
